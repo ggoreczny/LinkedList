c316305e23e9ab9b3af7b1105a1d7da0b198f233
grzegorz <g.goreczny@gmail.com>
2018-07-26 20:35:18 +0100

    Simple implementation of Linked List

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList<T>
+    {
+        private class Node<T>
+        {
+            public Node(T value)
+            {
+                Value = value;
+            }
+
+            public Node<T> NextNode { get; set; }
+            public T Value { get; }
+        }
+
+        private Node<T> head;
+
+        public T MiddleElement()
+        {
+            if (head == null)
+            {
+                return default(T);
+            }
+
+            var element = head;
+            var middleElement = head;
+
+            var isNextElementEven = false;
+            while (element.NextNode != null)
+            {
+                element = element.NextNode;
+                if (isNextElementEven)
+                {
+                    middleElement = middleElement.NextNode;
+                }
+
+                isNextElementEven = !isNextElementEven;
+            }
+            return middleElement.Value;
+        }
+
+        public void Add(T value)
+        {
+            if (head == null)
+            {
+                head = new Node<T>(value);
+                return;
+            }
+
+            var lastElement = head;
+            while(lastElement.NextNode != null)
+            {
+                lastElement = lastElement.NextNode;
+            }
+            lastElement.NextNode = new Node<T>(value);
+        }
+    }
+}
---
+++ LinkedListTests/LinkedList.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Linq;
+using NUnit.Framework;
+using LinkedList;
+
+namespace LinkedListTests
+{
+    [TestFixture]
+    public class LinkedList
+    {
+        [Test]
+        [TestCase(new [] {1,2,3}, 2)]
+        [TestCase(new[] { 1, 2, 3, 4}, 2)]
+        [TestCase(new[] { 1, 2, 3, 4, 5}, 3)]
+        [TestCase(new int [0], default(int))]
+        public void Given_Elements_When_Adding_To_LinkedList_Then_Middle_Element_Should_Be_As_Follow(int[] arrayOfElements, int expectedElement)
+        {
+            var linkedList = new LinkedList<int>();
+            arrayOfElements.ToList().ForEach(i => linkedList.Add(i));
+
+            var result = linkedList.MiddleElement();
+            Assert.That(result, Is.EqualTo(expectedElement), "Expected element at middle position: {0}", expectedElement);
+        }
+    }
+}
